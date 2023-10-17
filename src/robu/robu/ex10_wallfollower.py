7f67ed5ecc43eff1e838ccceffd840062183a0cc
kocran19 <kocran19@htl-kaindorf.at>
2023-10-17 13:09:13 +0200

    17.10.2023_3

--- src/robu/robu/ex10_wallfollower.py
+++ src/robu/robu/ex10_wallfollower.py
@@ -1,8 +1,7 @@
 import math
 
-def main():
-    print('Hi from robu.')
 
 
-if __name__ == '__main__':
-    main()
+def main(args=None):
+    print("Hello from Wallfollower!")
+    #pass ##Means do nosing, cause not knowing what todo
