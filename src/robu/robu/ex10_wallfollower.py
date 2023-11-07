9a4409c3bc06d69c733a1f2c618da97c1dfecfad
kocran19 <kocran19@htl-kaindorf.at>
2023-11-07 13:18:30 +0100

    07.11.2023

--- src/robu/robu/ex10_wallfollower.py
+++ src/robu/robu/ex10_wallfollower.py
@@ -1,9 +1,10 @@
 import math
 import rclpy
 
-from std_msg.msg import String
-from geometry_msg.msg import Twist
-from std_msgs.msg import LaserScan
+from rclpy.node import Node
+from std_msgs.msg import String
+from geometry_msgs.msg import Twist
+from sensor_msgs.msg import LaserScan
 
 from rclpy.qos import qos_profile_sensor_data
 
@@ -32,12 +33,12 @@ class WallFollowerStates(IntEnum):
     WF_STATE_FOLLOWWALL = 3
 
 
-class WallFollower(rclpy.Node):
+class WallFollower(Node):
     def __init__(self):
         super()._init__('WallFollower')
         self.scan_subscriber = self.create_subscription(LaserScan,"\scan", self.scan_callback, qos_profile_sensor_data)
 
-        self.cmd_vel_publisher = self.create_publisher(Twist, "\cmd_vel", qos_profiole_sensor_data)
+        self.cmd_vel_publisher = self.create_publisher(Twist, "\cmd_vel", qos_profile_sensor_data)
 
         self.left_dist = 9999999.9 #Intialisiere den Variable auf einen ungültigen Wert
         self.leftfront_dist = 9999999.9
@@ -45,6 +46,7 @@ class WallFollower(rclpy.Node):
         self.rightfront_dist = 9999999.9
         self.right_dist = 9999999.9
         self.rear_dist = 9999999.9
+        self.distances = []
 
         self.wallfollower_state = WallFollowerStates.WF_STATE_DETECTWALL
 
@@ -57,8 +59,34 @@ class WallFollower(rclpy.Node):
         self.dist_thresh_wf = 0.3
         self.dist_hysteresis_wf = 0.02
 
+        self.dist_laser_offset = 0.03
+        self.valid_lidar_data = False
         self.timer = self.create_timer(0.2, self.timer_callback)
 
+    def timer_callback(self):
+        if self.valid_lidar_data: #Beim erstmaligen Aufruf liegen noch keine LiDAR Daten vor
+            self.follow.wall()
+
+    def follow_wall(self):
+        msg =  Twist()
+        msg.linear.x = 0.0
+        msg.linear.y = 0.0
+        msg.linear.z = 0.0
+
+        msg.angular.x = 0.0
+        msg.angular.y = 0.0
+        msg.angular.z = 0.0
+
+        if self.wallfollower_state == WallFollowerStates.WF_STATE_DETECTWALL:
+            dist_min = min(self.distances)
+            if self.front_dist > (dist_min + self.dist_laser_offset):
+                if abs(self.front_dist - dist_min) < 0.2:
+                    msg.angular.z = self.turning_speed_wf_slow
+                else:
+                    msg.angular.z = self.turning_speed_wf_fast
+            else:
+                print("WF_SATE_DRIVE2WALL")
+                self.wallfollower_state = WallFollowerStates.WF_STATE_DRIVE2WALL
     def scan_callback(self, msg):
         self.left_dist = msg.ranges[ROBOT_DIRECTION_LEFT_INDEX]
         self.leftfront_dist = msg.ranges[ROBOT_DIRECTION_LEFT_FRONT_INDEX]
@@ -66,7 +94,9 @@ class WallFollower(rclpy.Node):
         self.rightfront_dist = msg.ranges[ROBOT_DIRECTION_RIGHT_FRONT_INDEX]
         self.right_dist = msg.ranges[ROBOT_DIRECTION_RIGHT_INDEX]
         self.rear_dist = msg.ranges[ROBOT_DIRECTION_REAR_INDEX]
+        self.distances = msg.ranges
 
+        self.valid_lidar_data = True
         print("ld: %.2f m" % self.left_dist,
               "ldf: %.2f m" % self.leftfront_dist,
               "fd: %.2f m" % self.front_dist,
@@ -86,4 +116,4 @@ def main(args=None):
 
     wallfollower.destroy_node()
     rclpy.shutdown()
-    #pass ##Means do nosing, cause not knowing what todo
+    #pass ##Means do nosing, cause not knowing what todo 
